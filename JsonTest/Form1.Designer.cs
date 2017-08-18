396eeae022be769c3a6b3b50f15d8cb0ebaa566c
oferyach <oferyach@gmail.com>
2017-08-18 10:16:12 +0300

    Init

---
+++ JsonTest/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace JsonTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Result = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(61, 113);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(715, 20);
+            this.Result.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 556);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ JsonTest/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Web;
+
+namespace JsonTest
+{
+    public partial class Form1 : Form
+    {
+
+        public class Employee
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            WebClient client = new WebClient();
+            string data = "";
+            client.Encoding = System.Text.Encoding.UTF8;
+
+            string reply = client.UploadString("http://ip.jsontest.com/?callback=showMyIP", data);
+
+            Result.Text = reply;
+            */
+
+            //////
+
+            Employee employee = new Employee
+            {
+                FirstName = "Jalpesh",
+                LastName = "Vadgama"
+            };
+
+            var javaScriptSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            string jsonString = javaScriptSerializer.Serialize(employee);
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://gurujsonrpc.appspot.com/guru");
+
+            
+            request.ContentType = "application/json; charset=utf-8";
+            request.Accept = "application/json, text/javascript, */*";
+            request.Method = "POST";
+            request.Headers.Add("Authorization","Bearer a503faf9-45b5-4fec-8334-337284a66ea4");
+            using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
+            {
+               // writer.Write("{id : 'test Orpak'}");
+                writer.Write(@"
+                {""StationId"" : 55, 
+                 ""RequestId"": ""e1234"" 
+""DateTime"": "" 2016-10-25 14:28:09.977"",
+""PumpId"": 2,
+""Fuel_type_code"": 1,
+""NR_ID"" : 3,
+""Cpass_UTN"": ""01234567""
+""TAGID"" : ""12340000"",
+""Odometer"":22,
+""Engine_Hours"" :7
+}
+");
+            }
+
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+
+            string json = "";
+
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    json += reader.ReadLine();
+                }
+            }
+
+            Result.Text = json;
+
+            /////
+
+        }
+    }
+}
